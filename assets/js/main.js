5603250904ce13f4c7b880e076189ae5c7844c01
screechovski <webdev1232@gmail.com>
2020-05-04 00:33:00 +0300

    Аякс запрос на сервер

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,57 @@
 "use strict";
 
-var hel = "hello";
+document.addEventListener("DOMContentLoaded", function () {
+
+    var serverRequest = function serverRequest(id) {
+        var serverResponse = 0;
+
+        var HttpClient = function HttpClient() {
+            this.get = function (aUrl, aCallback) {
+                var anHttpRequest = new XMLHttpRequest();
+                anHttpRequest.onreadystatechange = function () {
+                    if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200) aCallback(anHttpRequest.responseText);
+                };
+                anHttpRequest.open("GET", aUrl, true);
+                anHttpRequest.send(null);
+            };
+        };
+
+        var client = new HttpClient();
+
+        client.get('https://www.googleapis.com/youtube/v3/videos?id=' + id + '&key=AIzaSyCEJu3yEXjMI9yPm0vVK663v7kCrLrSg7k&part=statistics', function (response) {
+            buildHtml(JSON.parse(response).items[0].statistics);
+        });
+    };
+
+    var addRequest = function addRequest() {
+        var button = document.getElementById('formButton'),
+            input = document.getElementById('inputChanelId');
+
+        button.addEventListener('click', function (e) {
+            e.preventDefault();
+            serverRequest(input.value);
+        });
+    };
+
+    var buildHtml = function buildHtml(dataArray) {
+        var container = document.querySelector('.ul--wrapper');
+
+        container.innerHTML = "";
+
+        var ul = document.createElement('ul'),
+            li = document.createElement('li');
+        ul.classList = "list-group";
+
+        for (var item in dataArray) {
+            var _li = document.createElement('li');
+            _li.classList = "list-group-item";
+            _li.innerText = item + " - " + dataArray[item];
+
+            ul.append(_li);
+        }
+
+        container.append(ul);
+    };
+
+    addRequest();
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,63 @@
-let hel = "hello";
+document.addEventListener("DOMContentLoaded", function(){
+
+    const serverRequest = (id) => {
+        let serverResponse = 0;
+
+        var HttpClient = function () {
+            this.get = function (aUrl, aCallback) {
+                let anHttpRequest = new XMLHttpRequest();
+                anHttpRequest.onreadystatechange = function () {
+                    if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                        aCallback(anHttpRequest.responseText);
+                }        
+                anHttpRequest.open("GET", aUrl, true);
+                anHttpRequest.send(null);
+            }
+        }
+
+        let client = new  HttpClient();
+
+        client.get('https://www.googleapis.com/youtube/v3/videos?id='+ id +'&key=AIzaSyCEJu3yEXjMI9yPm0vVK663v7kCrLrSg7k&part=statistics', (response) => {
+            buildHtml(JSON.parse(response).items[0].statistics);
+        })
+    }
+
+    const addRequest = () => {
+        const button = document.getElementById('formButton'),
+            input = document.getElementById('inputChanelId');
+
+        button.addEventListener('click', function(e){
+            e.preventDefault();
+            serverRequest(input.value);
+        })
+    }
+
+    const buildHtml = (dataArray) => {
+        const container = document.querySelector('.ul--wrapper');
+
+        container.innerHTML = "";
+
+        let ul = document.createElement('ul'),
+        li = document.createElement('li');
+        ul.classList = "list-group";
+
+        for (var item in dataArray) {
+            let li = document.createElement('li');
+            li.classList = "list-group-item";
+            li.innerText = item + " - " + dataArray[item];
+
+            ul.append(li);
+        }
+
+        container.append(ul);
+
+    }
+
+    addRequest();
+
+
+    
+
+
+    
+});
